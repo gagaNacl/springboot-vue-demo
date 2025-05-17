dc66eb5e201d1fbcf1d0323603a01dd51a5c012f
chuhe <95133849+gagaNacl@users.noreply.github.com>
2025-05-17 21:23:43 +0800

    创建通用的 API 响应封装类

---
+++ backend/src/main/java/com/jiandu/backend/pojo/Result.java
@@ -0,0 +1,24 @@
+package com.jiandu.backend.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result<T> {
+    private Integer code;//状态码
+    private String message;//提示信息
+    private T data;//返回数据.
+
+    //快速返回操作成功响应结果（带响应数据）
+    public static <E> Result<E> success(E data) {
+        return new Result<>(200, "success", data);
+    }
+    //快速返回操作成功响应结果
+    public static Result success() {
+        return new Result(200, "success", null);
+    }
+    public static Result error() {
+        return new Result(500, "error", null);
+    }
+}
