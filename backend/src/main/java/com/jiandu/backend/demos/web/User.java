e0ee4342c8e04afe729a3965bfa40c8f66f9076b
chuhe <95133849+gagaNacl@users.noreply.github.com>
2025-05-17 20:54:27 +0800

    添加用户实体类

--- backend/src/main/java/com/jiandu/backend/demos/web/User.java
+++ backend/src/main/java/com/jiandu/backend/demos/web/User.java
@@ -1,43 +1,18 @@
-/*
- * Copyright 2013-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.jiandu.backend.demos.web;
 
-/**
- * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
- */
+
+import java.time.LocalDateTime;
+
 public class User {
 
-    private String name;
+    private Integer id;//主键id
+    private String name;//用户名
+    private String password;//用户密码
+    private String nickname;//用户昵称
+    private String email;//邮箱
+    private String userPic;//用户头像地址
+    private LocalDateTime createTime;//创建时间
+    private LocalDateTime updateTime;//更新时间
 
-    private Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
